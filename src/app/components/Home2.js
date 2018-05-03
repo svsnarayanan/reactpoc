c54c72e2b4fcfa7e81308f861a32a76c4f4ce2b7
reactjspoc <svsnarayanan@gmail.com>
2018-05-03 11:34:25 -0700

    adding files

--- src/app/components/Home2.js
+++ src/app/components/Home2.js
@@ -5,13 +5,55 @@ export class Home2 extends React.Component {
 		super();
 		this.state={
 			age : props.age,
-			status : 0
+			status : 0,
+			userName : props.name
+
 		}
 	}
+
+	/* Life cycle method 1 */
+	componentWillMount(){
+		console.log("Life cycle method 1 :: component will mount");
+	}
+	/* Life cycle method 2 */
+	componentDidMount(){
+		console.log("Life cycle method 2 :: component Did mount");
+	}
+
+	/* Life cycle method 3 */
+	componentWillReceiveProps(nprops){
+		console.log("Life cycle method 3 :: component componentWillReceiveProps",nprops);
+	}
+	/* Life cycle method 4 */
+	shouldComponentUpdate(nprops,nstate){
+		console.log("Life cycle method 4 :: component shouldComponentUpdate",nprops,nstate);
+		return true;
+	}
+	/* Life cycle method 5 */
+	componentWillUpdate(nprops,nstate){
+		console.log("Life cycle method 5 :: component Will Update",nprops,nstate);
+	}
+	/* Life cycle method 6 */
+	componentDidUpdate(pprops,pstate){
+		console.log("Life cycle method 6 :: component Will Update",pprops,pstate);
+	}
+	/* Life cycle method 7 */
+	componentWillUnmount(){
+		console.log("Life cycle method 7 ::  component Will Unmount");
+	}
+
 	btnOlder(){
 		this.setState ( {
 			age : this.state.age + 5
 		});	
+	}
+	onchangeName() {
+		this.props.changeName(this.state.userName);
+	}
+	onHangleTextChange(e){
+		this.setState({
+			userName : e.target.value
+		})
 	}
 	render(){
 		return (
@@ -20,6 +62,11 @@ export class Home2 extends React.Component {
 					<p> Your name is {this.props.name}</p>
 					<p> Your Age is {this.state.age}</p>
 					<button onClick={() => this.btnOlder()} className="btn btn-primary">Old-Age! </button>
+					<hr />
+					<button onClick={this.props.sayHello} className="btn btn-primary">Greeting</button>
+					<hr />
+					<input type="text" value = {this.state.userName} onChange={this.onHangleTextChange.bind(this)} />
+					<button  className="btn btn-primary" onClick={this.onchangeName.bind(this)}>Change Name</button>
 				</div>
 		);
 	}
@@ -27,5 +74,6 @@ export class Home2 extends React.Component {
 
 Home2.propTypes = {
 	name: PropTypes.string,
-	age : PropTypes.number
+	age : PropTypes.number,
+	sayHello : PropTypes.func
 }
--- src/app/index.js
+++ src/app/index.js
@@ -4,8 +4,25 @@ import { render } from "react-dom";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { Home2 } from "./components/Home2";
+import { HeaderSL } from "./components/HeaderSL";
+
 
 class FirstReactApp extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			userName : "Tyrion Lannister"
+		}
+	}
+	onHelloClick(){
+			alert('hi welcome to index.js from home .js');
+	}
+	onChangeFname(newName){
+		this.setState({
+			userName : newName
+		})
+
+	}
 	render(){
 		var userInfo = {
 			firstname : "john",
@@ -13,6 +30,7 @@ class FirstReactApp extends React.Component {
 			hobbies : ["Skating","Hiking","Biking"],
 			email : "john@smith.com"
 		};
+		
 		return (
 				<div className="container">
 					<div className="row">
@@ -22,14 +40,24 @@ class FirstReactApp extends React.Component {
 					</div>
 					<div className="row">
 						<div className="col-xs-10 col-xs-offset-1">
-							<h1><Home name="John smith" age="33" user={userInfo}>
+							<h1><Home name={this.state.userName} age="33" user={userInfo}>
 								<p>Home component rendered successfully</p>
 							</Home></h1>
 						</div>
 					</div>
 					<div className="row">
 						<div className="col-xs-10 col-xs-offset-1">
-							<h1><Home2 name="John smith Home-2" age={33} /></h1>
+							<h1><Home2 
+										name={this.state.userName} 
+										age={33} 
+										sayHello={this.onHelloClick} 
+										changeName = {this.onChangeFname.bind(this)}
+										/></h1>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-xs-10 col-xs-offset-1">
+							<h1><HeaderSL linkText="Here is just a state-less components" /></h1>
 						</div>
 					</div>
 				</div>
